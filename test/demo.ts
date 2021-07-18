dac7642887916831629fc3845f28123035bb87b1
wuliang.lance <wuliang.lance@bytedance.com>
2021-07-18 21:39:43 +0800

    feat: support commit message

--- test/demo.ts
+++ test/demo.ts
@@ -3,9 +3,11 @@
 import fetchIdl from '../src/index';
 
 const repository = 'git@github.com:lancewuz/fetch-idl.git';
-fetchIdl(
+const res = fetchIdl(
   repository,
   'refactor-loose-dependent',
   'test/idl/index.proto',
   `test/temp`
 );
+
+console.log(res);
